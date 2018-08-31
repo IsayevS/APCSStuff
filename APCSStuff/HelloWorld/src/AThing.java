4ad80c8321f7b05e4af2e0f6f441427fb92a5706
Sergey Isayev <serega.isayev@gmail.com>
2018-08-31 09:51:33 -0700

    Something

---
+++ APCSStuff/HelloWorld/src/AThing.java
@@ -0,0 +1,15 @@
+
+public class AThing {
+	/**
+	* @author Sergey Isayev
+	* @version 8/17/2018
+	*/
+	public static void main(String[] args) {
+		
+		    {
+		    	
+		        System.out.println("Hello World! From Sergey");
+		    }
+		}
+
+	}
